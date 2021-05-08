721ec966a36fec6788e4698435349598430e987b
Neil Lawrence <lawrennd@gmail.com>
2021-05-08 09:16:27 +0100

    Add pip install files.

---
+++ README.md
@@ -0,0 +1,2 @@
+# MLAI Is a package of python files for teaching and lecturing on machine learning.
+
---
+++ mlai/__init__.py
@@ -0,0 +1,6 @@
+from . import mlai
+from . import plot
+from . import gp_plot
+from . import mountain_car
+from . import gp_tutorial
+from . import deepgp_tutorial
