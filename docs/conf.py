10791c0f797d86b5de2c2d5066dc74b0affec8f1
Neil Lawrence <lawrennd@gmail.com>
2025-07-09 07:09:14 +0100

    Set up Sphinx documentation with comprehensive structure, installation guide, quickstart, contributing guide, and project tenets integration

---
+++ docs/conf.py
@@ -0,0 +1,78 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'MLAI'
+copyright = '2018-2025, Neil D. Lawrence'
+author = 'Neil D. Lawrence'
+version = '0.1.2'
+release = '0.1.2'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+    'sphinx.ext.napoleon',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.intersphinx',
+    'sphinx_rtd_theme',
+    'sphinx_autodoc_typehints',
+    'myst_parser',
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+language = 'en'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# -- Options for autodoc ----------------------------------------------------
+autodoc_default_options = {
+    'members': True,
+    'member-order': 'bysource',
+    'special-members': '__init__',
+    'undoc-members': True,
+    'exclude-members': '__weakref__'
+}
+
+# -- Options for intersphinx -------------------------------------------------
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'numpy': ('https://numpy.org/doc/stable/', None),
+    'pandas': ('https://pandas.pydata.org/docs/', None),
+    'matplotlib': ('https://matplotlib.org/stable/', None),
+}
+
+# -- Options for mathjax ----------------------------------------------------
+mathjax_config = {
+    'TeX': {
+        'equationNumbers': {'autoNumber': 'AMS'},
+    }
+}
+
+# -- Options for Napoleon ---------------------------------------------------
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = False
+napoleon_include_private_with_doc = False
+napoleon_include_special_with_doc = True
+napoleon_use_admonition_for_examples = False
+napoleon_use_admonition_for_notes = False
+napoleon_use_admonition_for_references = False
+napoleon_use_ivar = False
+napoleon_use_param = True
+napoleon_use_rtype = True
+napoleon_preprocess_types = False
+napoleon_type_aliases = None
+napoleon_attr_annotations = True
---
+++ docs/tutorials/index.rst
@@ -0,0 +1,31 @@
+Tutorials
+=========
+
+This section contains tutorials and examples that demonstrate how to use MLAI for learning machine learning concepts.
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Tutorials:
+
+   gp_tutorial
+   deepgp_tutorial
+   mountain_car
+
+Getting Started
+--------------
+
+If you're new to MLAI, we recommend starting with the :doc:`../quickstart` guide before diving into these tutorials.
+
+Each tutorial is designed to be educational and includes:
+
+- Clear explanations of the underlying concepts
+- Step-by-step code examples
+- Mathematical background where relevant
+- Visualizations to aid understanding
+
+Tutorial Structure
+-----------------
+
+- *Gaussian Process Tutorial*: Introduction to Gaussian Processes
+- *Deep GP Tutorial*: Advanced concepts with Deep Gaussian Processes  
+- *Mountain Car Example*: Reinforcement learning with the mountain car environment 
